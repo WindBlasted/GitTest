0d6979113dfdc51cf3d1eb878e4f2cb0c11ca967
Cedric <WindBlasted@cedric.michel-lepage@hotmail.com>
2019-03-01 09:38:55 +0100

    Ajout de truc

--- AppliPourGit/Employe.cs
+++ AppliPourGit/Employe.cs
@@ -10,11 +10,35 @@ namespace AppliPourGit
     {
         private string nom;
         private string prenom;
+        private int id;
+        private string libelle;
 
-        public void contructEmploye(string pnom, string pprenom)
+        public void contructEmploye(string pnom, string pprenom, int pid, string plibelle)
         {
             this.prenom = pprenom;
             this.nom = pnom;
+            this.id = pid;
+            this.libelle = plibelle;
+        }
+
+        public string getNom()
+        {
+            return this.nom;
+        }
+
+        public string getPrenom()
+        {
+            return this.prenom;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public string getLibelle()
+        {
+            return this.libelle;
         }
     }
 }
