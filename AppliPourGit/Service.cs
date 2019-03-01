2852d6d9459db4119bd01da4c4bfc899dc9622a7
Auchericou <48093093+Auchericou@users.noreply.github.com>
2019-03-01 09:40:23 +0100

    Ajout classe Service

---
+++ AppliPourGit/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliPourGit
+{
+    class Service
+    {
+        private int id;
+        private string libelle;
+
+        public void newServ(int pid, string plibelle)
+        {
+            this.id = pid;
+            this.libelle = plibelle;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public void setLibelle(string unLibelle)
+        {
+            libelle = unLibelle;
+        }
+        
+    }
+}
