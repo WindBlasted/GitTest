a14b4949088b616c9c4b6fb050654e834fa5ae27
ousmanebad <48093066+ousmanebad@users.noreply.github.com>
2019-03-01 09:48:18 +0100

    formservice

---
+++ AppliPourGit/FsaisieService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppliPourGit
+{
+    public partial class FsaisieService : Form
+    {
+        public FsaisieService()
+        {
+            InitializeComponent();
+        }
+
+        private void FsaisieService_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblTitre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TbLibelle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
